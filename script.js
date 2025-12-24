c13190f0a12b96d0e47ce737a860903b88d394f0
punyatwaya <nayakpunatoya@gmail.com>
2025-12-24 20:33:12 +0530

    update

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-let sections= document.querySelectorAll('section');
-let navLinks= document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-     let offset = sec.offsetTop - 150;
-     let height = sec.offsetHeight;
-     let id = sec.getAttribute('id');
-     
-     if(top >= offset && top < offset + height){
-        navLinks.forEach(link => {
-            navLinks.classList.remove('active');
-            document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-        });
+    let top = window.scrollY;
 
-     };
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document
+                .querySelector('header nav a[href*=' + id + ']')
+                .classList.add('active');
+        }
     });
+
     // sticky navbar
     let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100);
+    header.classList.toggle('sticky', top > 100);
 };
+
+// mobile menu toggle
 menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
